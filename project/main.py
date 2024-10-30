a0c3cf8f6080ec89ff098df1e0a546bb672e893b
Aidan Feess <aidanfeess@gmail.com>
2024-10-30 10:52:09 -0500

    Updated code for android app and scanning functionality

--- project/main.py
+++ project/main.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask_cors import CORS
 import base64
 import re
 import os
-from image_rec import recognize_qr_code  # Import the QR code recognition function
+import vt
+from image_rec import recognize_qr_code
+
+VT_API_KEY = '' # virus total api key
 
 app = Flask(__name__)
+CORS(app)
 
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
@@ -33,11 +38,21 @@ def upload_image():
             f.write(image)
 
         # Use OpenCV to check for QR code
-        qr_present = recognize_qr_code(image_path)
+        qr_content = recognize_qr_code(image_path)
+        qr_present = False if not qr_content else True
+        qr_analysis = "Safe!"
+        if qr_content:
+            with vt.Client(VT_API_KEY) as client:
+                url = qr_content
+                qr_analysis = client.scan_url(url)
+        
+        os.remove(image_path)
 
         return jsonify({
             'status': 'success',
             'qr_code_detected': qr_present,
+            'qr_code_url': qr_content,
+            'qr_code_analysis': qr_analysis,
             'image_url': f'/uploads/captured_image.png'
         })
     except Exception as e:
@@ -48,4 +63,4 @@ def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
