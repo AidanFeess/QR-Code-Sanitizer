2627416d0673a64a414ccdd4aceadea24c257007
Aidan Feess <145503050+AidanFeess@users.noreply.github.com>
2024-11-04 10:53:21 -0600

    updated back end to display analysis data

--- project/image_rec.py
+++ project/image_rec.py
@@ -9,7 +9,11 @@ def recognize_qr_code(image_path):
         image = cv2.imread(image_path)
         detector = cv2.QRCodeDetector()
         decoded_text, points, _ = detector.detectAndDecode(image)
-        return decoded_text if decoded_text else None
+        isQR = True
+        if not decoded_text:
+            isQR = False
+
+        return isQR, decoded_text
     except Exception as e:
         print(f"Error detecting QR code: {e}")
         return False
--- project/main.py
+++ project/main.py
@@ -8,13 +8,41 @@ from image_rec import recognize_qr_code  # Import the QR code recognition functi
 
 vt_key = ''
 
+def get_analysis(analysis_id):
+    # define URL for the vt API analysis endpoint
+    analysis_url = f"https://www.virustotal.com/api/v3/analyses/{analysis_id}"
+    headers = {
+        "x-apikey": vt_key
+    }
+    response = requests.get(analysis_url, headers=headers)
+    
+    if response.status_code == 200:
+        analysis_data = response.json()
+        
+        # display results in readable format
+        stats = analysis_data['data']['attributes']['stats']
+        print("Malicious:", stats['malicious'])
+        print("Suspicious:", stats['suspicious'])
+        print("Harmless:", stats['harmless'])
+        print("Undetected:", stats['undetected'])
+
+        danger_type = "Harmless"
+        if stats['malicious'] > 0:
+            danger_type = "Malicious"
+        elif stats['suspicious'] > 0:
+            danger_type = "Suspicious"
+        elif stats['harmless'] == 0:
+            danger_type = "Undetected"
+        
+        return analysis_data, danger_type
+    else:
+        print("Error:", response.status_code)
+        return None
+
 def scan_url(url_to_scan):
     global vt_key
     # Define the URL for the VirusTotal API endpoint
     vt_url = "https://www.virustotal.com/api/v3/urls"
-    
-    # Encode the URL to be scanned (VirusTotal requires URL-safe base64 encoding)
-    url_id = base64.urlsafe_b64encode(url_to_scan.encode()).decode().strip("=")
 
     # Set up headers with the API key
     headers = {
@@ -30,7 +58,8 @@ def scan_url(url_to_scan):
     
     # Check if the request was successful
     if response.status_code == 200:
-        return response.json()  # Return JSON data for the scan result
+        analysis_id = response.json()['data']['id']
+        return get_analysis(analysis_id)  # Return JSON data for the scan result
     else:
         print("Error:", response.status_code)
         return None
@@ -65,11 +94,13 @@ def upload_image():
 
         # Use OpenCV to check for QR code
         qr_present, qr_text = recognize_qr_code(image_path)
-        print(scan_url(qr_text))
+        if qr_present:
+            qr_all_data, qr_simple_data = scan_url(qr_text)
 
         return jsonify({
             'status': 'success',
-            'qr_code_detected': qr_text,
+            'qr_code_detected': qr_present,
+            'qr_danger_status': qr_simple_data,
             'image_url': f'/uploads/captured_image.png'
         })
     except Exception as e:
